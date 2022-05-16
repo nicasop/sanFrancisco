d0ba9bea9899ce40242aa2daa1c6ee9cd91ccbba
nicasop <nicolas_villita21@hotmail.es>
2022-05-16 06:30:31 -0500

    cambios catalogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HistoriaComponent } from './historia/historia.component';
 import { FooterComponent } from './footer/footer.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { ProductoComponent } from './producto/producto.component';
+
+const rutas:Route[]=[
+  {path: 'historia', component:HistoriaComponent},
+  {path: 'catalogo', component:CatalogoComponent},
+  // {path: 'producto', component:ReporteComponent}
+]; 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HistoriaComponent,
-    FooterComponent
+    FooterComponent,
+    CatalogoComponent,
+    ProductoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
